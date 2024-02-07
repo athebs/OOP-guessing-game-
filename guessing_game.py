635da32af136eb189d37dc8dbf2346c5bab3f044
athebs <athena.s.tafur@gmail.com>
2024-02-06 18:37:39 -0600

    first crack at it

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,15 @@
 class GuessingGame():
+    def __init__(self, user_guess, answer=12) -> None:
+        self._user_guess = user_guess 
+        self._answer = answer
+    
+    def guess(self, user_guess):
+        if user_guess != answer:
+            if user_guess > answer:
+                print("lower!")
+            elif user_guess < answer:
+                print("higher!")
+            else:
+            
+    #a method of determining if the user was able 
+        
